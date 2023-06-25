9f6d6f8e95c0e7d2af68e709164e5c531a8f465d
Jasdeep Singh <singhjasdeep1602@gmail.com>
2023-06-25 17:21:31 +0530

    Nike1

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -1,8 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit'
+import toast from 'react-hot-toast'
 
 const initialState = {
   cartState: false,
-  cartItems: [],
+  cartItems: localStorage.getItem('cart')
+    ? JSON.parse(localStorage.getItem('cart'))
+    : [],
 }
 
 const CartSlice = createSlice({
@@ -16,7 +19,20 @@ const CartSlice = createSlice({
       state.cartState = action.payload.cartState
     },
     setAddItemToCart: (state, action) => {
-      state.cartItems.push(action.payload)
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      )
+
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuantity += 1
+        toast.success(`Item QTY Increased`)
+      } else {
+        const temp = { ...action.payload, cartQuantity: 1 }
+        state.cartItems.push(temp)
+
+        toast.success(`${action.payload.title} added to Cart`)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cartItems))
     },
   },
 })
